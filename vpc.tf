a7532f474fc8e59f24cfe68fc5de77b1a1dbe989
Ahmed-Ali-Elbaz <112098369+Ahmed-Ali-Elbaz@users.noreply.github.com>
2022-11-14 18:44:08 +0200

    Delete vpc.tf

--- vpc.tf
+++
@@ -1,8 +0,0 @@
-# Create a VPC network
-resource "google_compute_network" "vpc_network" {
-  name                    = "vpc-network"
-  auto_create_subnetworks = false
-  mtu                     = 1460
-}
-
-
